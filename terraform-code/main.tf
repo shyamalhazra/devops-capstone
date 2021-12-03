f77af5a6b757c1b255893efc5dc77c1f90b11b62
Shyamal Hazra <shyamalhazra02@gmail.com>
2021-12-03 17:33:47 +0530

    Update main.tf

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,10 +1,14 @@
 resource "aws_instance" "nadeploymentServers" {
-	count=var.no_of_instances
+    count=var.no_of_instances
     ami = var.ami_id
+    availability_zone = var.zones[count.index]
     instance_type = var.instance_type
-    vpc_security_group_ids = ["sg-09805759766f1bc31"]
+    vpc_security_group_ids = ["sg-0bb9734690fba169a"]
     key_name = "sample_ubuntu"
-
+	
+    tags = {
+    Name = "Server-${count.index +1}"
+  	}
     connection {
       host = self.public_ip
       type = "ssh"
